f2211e41b9964a323b3e0cd51f8ac4e35f5116af
Billyngwa <billytatangwa5@gmail.com>
2022-09-17 19:50:43 -0700

    start of the media queries

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+var hamburger = document.getElementById('hamburger_btn');
+var navigation = document.getElementById('nav_panel');
+hamburger.addEventListener('click', function(){
+    if(navigation.style.display=='none'){
+        navigation.style.display = 'block';
+    }else{
+        navigation.style.display ='none';
+    }
+})
