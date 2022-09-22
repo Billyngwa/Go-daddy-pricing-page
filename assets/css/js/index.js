085ed606ddba5c67ec16773654e2896995023781
Billyngwa <billytatangwa5@gmail.com>
2022-09-21 22:38:12 -0700

    completion of mobile desig 80%

--- assets/css/js/index.js
+++ assets/css/js/index.js
@@ -1,10 +1,20 @@
 alert = 'hello world';
 var hamburger = document.getElementById('hamburger_btn');
 var navigation = document.getElementById('nav_panel');
+var sectionFour = document.getElementsById('section-four-relative');
+var sectionFourAbsolute = document.getElementsById('scetion-four-absolute');
 hamburger.addEventListener('click', function(){
     if(navigation.style.display=='none'){
         navigation.style.display = 'block';
     }else{
         navigation.style.display ='none';
+    }
+})
+
+sectionFour.addEventListener('click', function(){
+    if(sectionFourAbsolute.style.display=='block'){
+        sectionFourAbsolute.style.display = 'none';
+    }else{
+        sectionFourAbsolute.style.display = 'block';
     }
 })
