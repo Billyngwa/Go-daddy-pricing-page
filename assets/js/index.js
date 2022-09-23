0b717ce8c75ff571ef3cccab7a9a1bf474caaff0
Billyngwa <billytatangwa5@gmail.com>
2022-09-22 20:47:51 -0700

    addition of section 4 js

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,3 +7,104 @@ hamburger.addEventListener('click', function(){
         navigation.style.display ='none';
     }
 })
+
+var whatIs = document.getElementById('what_is');
+var whatIsAbsolute = document.getElementById('what_is_absolute');
+whatIs.addEventListener('click', function(){
+    if(whatIsAbsolute.style.display=='none'){
+        whatIsAbsolute.style.display = 'block';
+    }else{
+        whatIsAbsolute.style.display ='none';
+    }
+})
+
+var howDo = document.getElementById('how_do');
+var howDoAbsolute = document.getElementById('how_do_absolute');
+howDo.addEventListener('click', function(){
+    if(howDoAbsolute.style.display=='none'){
+        howDoAbsolute.style.display = 'block';
+    }else{
+        howDoAbsolute.style.display ='none';
+    }
+})
+
+var iHave = document.getElementById('i_have');
+var iHaveAbsolute = document.getElementById('i_have_absolute');
+iHave.addEventListener('click', function(){
+    if(iHaveAbsolute.style.display=='none'){
+        iHaveAbsolute.style.display = 'block';
+    }else{
+        iHaveAbsolute.style.display ='none';
+    }
+})
+
+var domainPrivacy = document.getElementById('domain_privacy');
+var domainPrivacyAbsolute = document.getElementById('domain_privacy_absolute');
+domainPrivacy.addEventListener('click', function(){
+    if(domainPrivacyAbsolute.style.display=='none'){
+        domainPrivacyAbsolute.style.display = 'block';
+    }else{
+        domainPrivacyAbsolute.style.display ='none';
+    }
+})
+
+
+var howMany = document.getElementById('how_many');
+var howManyAbsolute = document.getElementById('how_many_absolute');
+howMany.addEventListener('click', function(){
+    if(howManyAbsolute.style.display=='none'){
+        howManyAbsolute.style.display = 'block';
+    }else{
+        howManyAbsolute.style.display ='none';
+    }
+})
+
+var canI = document.getElementById('can_i');
+var canIAbsolute = document.getElementById('can_i_absolute');
+canI.addEventListener('click', function(){
+    if(canIAbsolute.style.display=='none'){
+        canIAbsolute.style.display = 'block';
+    }else{
+        canIAbsolute.style.display ='none';
+    }
+})
+
+var howMuch = document.getElementById('how_much');
+var howMuchAbsolute = document.getElementById('how_much_absolute');
+howMuch.addEventListener('click', function(){
+    if(howMuchAbsolute.style.display=='none'){
+        howMuchAbsolute.style.display = 'block';
+    }else{
+        howMuchAbsolute.style.display ='none';
+    }
+})
+
+var bestDomain = document.getElementById('best_domain');
+var bestDomainAbsolute = document.getElementById('best_domain_absolute');
+bestDomain.addEventListener('click', function(){
+    if(bestDomainAbsolute.style.display=='none'){
+        bestDomainAbsolute.style.display = 'block';
+    }else{
+        bestDomainAbsolute.style.display ='none';
+    }
+})
+
+var alreadyTaken = document.getElementById('already_taken');
+var alreadyTakenAbsolute = document.getElementById('already_taken_absolute');
+alreadyTaken.addEventListener('click', function(){
+    if(alreadyTakenAbsolute.style.display=='none'){
+        alreadyTakenAbsolute.style.display = 'block';
+    }else{
+        alreadyTakenAbsolute.style.display ='none';
+    }
+})
+
+var registerMore = document.getElementById('register_more');
+var registerMoreAbsolute = document.getElementById('register_more_absolute');
+registerMore.addEventListener('click', function(){
+    if(registerMoreAbsolute.style.display=='none'){
+        registerMoreAbsolute.style.display = 'block';
+    }else{
+        registerMoreAbsolute.style.display ='none';
+    }
+})
